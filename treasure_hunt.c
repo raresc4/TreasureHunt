fc7009a0a65885eb0306c3c6344f21f88048c495
raresc4 <rarescatana4@gmail.com>
2025-03-31 21:11:36 +0300

    added basic operations

--- treasure_hunt.c
+++ treasure_hunt.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <unistd.h>
 
 typedef struct {
     int x;
@@ -18,12 +19,17 @@ typedef struct {
 }Treasure;
 
 Treasure createTreasure() {
+    printf("Introdu id-ul:\n");
     Treasure treasure;
     scanf("%d", &treasure.id);
+    printf("Introdu username-ul:\n");
     scanf("%s", treasure.username);
+    printf("Introdu coordonatele:\n");
     scanf("%d", &treasure.coordonate.x);
     scanf("%d", &treasure.coordonate.y);
+    printf("Introdu indiciul:\n");
     scanf("%s", treasure.clue);
+    printf("Introdu valoarea:\n");
     scanf("%d", &treasure.value);
     return treasure;
 }
@@ -34,29 +40,177 @@ void showTreasure(Treasure *tr) {
 
 void add(char *hunt) {
     DIR *dir = opendir(hunt);
-    char newPath[100];
+    char newDirPath[100];
     int new = 0;
     if(!dir) {
         new = 1;
-        sprintf(newPath,"./%s",hunt);
-        if(mkdir(newPath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) < 0) {
+        sprintf(newDirPath,"./%s",hunt);
+        if(mkdir(newDirPath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) < 0) {
             perror("eroarea la crearea noului hunt");
             return;
         }
     }
-    // struct dirent *fisier = NULL;
-    // int exista = 0;
-    // Treasure tr = createTreasure();
-    // char name[100];
-    // sprintf(name,"hunt%d",tr.id);
-    // FILE *f = fopen('')
+    if(new) {
+        dir = opendir(newDirPath);
+        if(!dir) {
+            perror("eroare la deschiderea directorului nou creat");
+            return;
+        }
+    }
+    char filepath[150];
+    if(new) {
+        sprintf(filepath, "%s/%s", newDirPath, "treasures");
+    } else {
+        sprintf(filepath, "%s/treasures", hunt);    
+    }
+    FILE *f = fopen(filepath, "ab+");
+    if(!f) {
+        perror("eroare la deschiderea sau crearea fisierului treasures");
+        return;
+    }
+    Treasure tr = createTreasure();
+    if(fwrite(&tr, sizeof(Treasure), 1, f) != 1) {
+        perror("eroare la scrierea in fisierul treasures");
+        return;
+    }
+    if(fclose(f)) {
+        perror("eroare la inchiderea fisierului treasures");
+        return;
+    }
     if(closedir(dir) < 0) {
         perror("eroare la inchiderea directorului");
         return;
     }
+    putchar('\n');
+}
+
+void list(char *hunt) {
+    char filepath[150];
+    sprintf(filepath, "%s/treasures", hunt);
+    FILE *f = fopen(filepath, "rb");
+    if(!f) {
+        perror("acest hunt nu exista");
+        return;
+    }
+    Treasure tr;
+    printf("Lista comorilor de la huntul %s:\n", hunt);
+    while(fread(&tr, sizeof(Treasure), 1, f)) {
+        showTreasure(&tr);
+    }
+    if(fclose(f)) {
+        perror("eroare la inchiderea fisierului treasures");
+        return;
+    }
+    putchar('\n');
+}
+
+void view(char *hunt, int id) {
+    char filepath[150];
+    sprintf(filepath, "%s/treasures", hunt);
+    FILE *f = fopen(filepath, "rb");
+    if(!f) {
+        perror("acest hunt nu exista");
+        return;
+    }
+    Treasure tr;
+    int found = 0;
+    while(fread(&tr, sizeof(Treasure), 1, f)) {
+        if(tr.id == id) {
+            showTreasure(&tr);
+            found = 1;
+            break;
+        }
+    }
+    if(!found) {
+        printf("Comoara cu id ul %d nu exista\n", id);
+    }
+    if(fclose(f)) {
+        perror("eroare la inchiderea fisierului treasures");
+        return;
+    }
+    putchar('\n');
+}
+
+void remove_hunt(char *hunt) {
+    char filepath[150];
+    sprintf(filepath, "%s/treasures", hunt);
+    if(remove(filepath) < 0) {
+        perror("eroare la stergerea fisierului treasures");
+        return;
+    }
+    if(rmdir(hunt) < 0) {
+        perror("eroare la stergerea directorului");
+        return;
+    }
+    putchar('\n');
+}
+
+void remove_treasure(char *hunt, int id) {
+    char filepath[150];
+    sprintf(filepath, "%s/treasures", hunt);
+    FILE *f = fopen(filepath, "rb+");
+    if(!f) {
+        perror("eroare la deschiderea fisierului treasures");
+        return;
+    }
+    Treasure tr;
+    FILE *temp = fopen("temp", "wb+");
+    if(!temp) {
+        perror("eroare la deschiderea fisierului temporar");
+        fclose(f);
+        return;
+    }
+    int found = 0;
+    while(fread(&tr, sizeof(Treasure), 1, f)) {
+        if(tr.id == id) {
+            found = 1;
+            continue;
+        }
+        if(fwrite(&tr, sizeof(Treasure), 1, temp) != 1) {
+            perror("eroare la scrierea in fisierul temporar");
+            fclose(f);
+            fclose(temp);
+            return;
+        }
+    }
+    if(!found) {
+        printf("Comoara cu id ul %d nu exista\n", id);
+        if(fclose(f)) {
+            perror("eroare la inchiderea fisierului treasures");
+            fclose(temp);
+            return;
+        }
+        if(fclose(temp)) {
+            perror("eroare la inchiderea fisierului temporar");
+            return;
+        }
+        remove("temp");
+        return;
+    }
+    if(fclose(f)) {
+        perror("eroare la inchiderea fisierului treasures");
+        fclose(temp);
+        return;
+    }
+    if(fclose(temp)) {
+        perror("eroare la inchiderea fisierului temporar");
+        return;
+    }
+    if(remove(filepath) < 0) {
+        perror("eroare la stergerea fisierului treasures");
+        return;
+    }
+    if(rename("temp", filepath) < 0) {
+        perror("eroare la redenumirea fisierului temporar");
+        return;
+    }
+    putchar('\n');
 }
 
 int main(int argc,char **argv) {
-add("hunt2");
+//add("hunt2");
+remove_treasure("hunt2", 2);
+list("hunt2");
+//remove_hunt("hunt2");
 return 0;
 }
