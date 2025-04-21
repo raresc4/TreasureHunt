fc63dbd327881df963be9876c50929b3f858bd00
raresc4 <rarescatana4@gmail.com>
2025-04-21 12:38:22 +0300

    initial commit for stage 2

---
+++ treasure_hub.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <features.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/signal.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+int main() {
+    
+    return 0;
+}
